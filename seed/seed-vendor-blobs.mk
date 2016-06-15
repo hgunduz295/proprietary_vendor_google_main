0bcd6c4043a842f82136c13f138616a709db80d8
Arvin Quilao <arquilao@gmail.com>
2016-06-15 15:13:55 +0800

    seed: Update proprietary libraries
    * Enable CNE/DPM/QC location
    
    Ref: https://github.com/TheMuppets/proprietary_vendor_wileyfox/commit/8243438a23b399258eb76110699a7cfdcb8a2576

--- seed/seed-vendor-blobs.mk
+++ seed/seed-vendor-blobs.mk
@@ -120,6 +120,30 @@ PRODUCT_COPY_FILES += \
     vendor/google/seed/proprietary/etc/firmware/cpp_firmware_v1_1_6.fw:system/etc/firmware/cpp_firmware_v1_1_6.fw \
     vendor/google/seed/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
     vendor/google/seed/proprietary/etc/firmware/cpp_firmware_v1_4_0.fw:system/etc/firmware/cpp_firmware_v1_4_0.fw \
+    vendor/google/seed/proprietary/bin/cnd:system/bin/cnd \
+    vendor/google/seed/proprietary/bin/dpmd:system/bin/dpmd \
+    vendor/google/seed/proprietary/etc/cne/andsfCne.xml:system/etc/cne/andsfCne.xml \
+    vendor/google/seed/proprietary/etc/cne/SwimConfig.xml:system/etc/cne/SwimConfig.xml \
+    vendor/google/seed/proprietary/etc/dpm/dpm.conf:system/etc/dpm/dpm.conf \
+    vendor/google/seed/proprietary/etc/dpm/nsrm/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml \
+    vendor/google/seed/proprietary/etc/permissions/cneapiclient.xml:system/etc/permissions/cneapiclient.xml \
+    vendor/google/seed/proprietary/etc/permissions/com.qti.dpmframework.xml:system/etc/permissions/com.qti.dpmframework.xml \
+    vendor/google/seed/proprietary/etc/permissions/com.quicinc.cne.xml:system/etc/permissions/com.quicinc.cne.xml \
+    vendor/google/seed/proprietary/etc/permissions/ConnectivityExt.xml:system/etc/permissions/ConnectivityExt.xml \
+    vendor/google/seed/proprietary/etc/permissions/dpmapi.xml:system/etc/permissions/dpmapi.xml \
+    vendor/google/seed/proprietary/framework/cneapiclient.jar:system/framework/cneapiclient.jar \
+    vendor/google/seed/proprietary/framework/com.qti.dpmframework.jar:system/framework/com.qti.dpmframework.jar \
+    vendor/google/seed/proprietary/framework/com.quicinc.cne.jar:system/framework/com.quicinc.cne.jar \
+    vendor/google/seed/proprietary/framework/ConnectivityExt.jar:system/framework/ConnectivityExt.jar \
+    vendor/google/seed/proprietary/framework/dpmapi.jar:system/framework/dpmapi.jar \
+    vendor/google/seed/proprietary/vendor/lib/libcne.so:system/vendor/lib/libcne.so \
+    vendor/google/seed/proprietary/vendor/lib/libcneapiclient.so:system/vendor/lib/libcneapiclient.so \
+    vendor/google/seed/proprietary/vendor/lib/libcneconn.so:system/vendor/lib/libcneconn.so \
+    vendor/google/seed/proprietary/vendor/lib/libcneqmiutils.so:system/vendor/lib/libcneqmiutils.so \
+    vendor/google/seed/proprietary/vendor/lib/libdpmframework.so:system/vendor/lib/libdpmframework.so \
+    vendor/google/seed/proprietary/vendor/lib/libdpmnsrm.so:system/vendor/lib/libdpmnsrm.so \
+    vendor/google/seed/proprietary/vendor/lib/libNimsWrap.so:system/vendor/lib/libNimsWrap.so \
+    vendor/google/seed/proprietary/vendor/lib/libvendorconn.so:system/vendor/lib/libvendorconn.so \
     vendor/google/seed/proprietary/bin/qseecomd:system/bin/qseecomd \
     vendor/google/seed/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
     vendor/google/seed/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
@@ -132,11 +156,24 @@ PRODUCT_COPY_FILES += \
     vendor/google/seed/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
     vendor/google/seed/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/google/seed/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/google/seed/proprietary/bin/loc_launcher:system/bin/loc_launcher \
+    vendor/google/seed/proprietary/etc/permissions/com.qti.location.sdk.xml:system/etc/permissions/com.qti.location.sdk.xml \
+    vendor/google/seed/proprietary/etc/permissions/com.qualcomm.location.xml:system/etc/permissions/com.qualcomm.location.xml \
+    vendor/google/seed/proprietary/framework/com.qti.location.sdk.jar:system/framework/com.qti.location.sdk.jar \
+    vendor/google/seed/proprietary/vendor/bin/slim_daemon:system/vendor/bin/slim_daemon \
     vendor/google/seed/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
+    vendor/google/seed/proprietary/vendor/lib/libdataitems.so:system/vendor/lib/libdataitems.so \
     vendor/google/seed/proprietary/vendor/lib/libflp.so:system/vendor/lib/libflp.so \
     vendor/google/seed/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
     vendor/google/seed/proprietary/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
     vendor/google/seed/proprietary/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
+    vendor/google/seed/proprietary/vendor/lib/liblocationservice_glue.so:system/vendor/lib/liblocationservice_glue.so \
+    vendor/google/seed/proprietary/vendor/lib/liblocationservice.so:system/vendor/lib/liblocationservice.so \
+    vendor/google/seed/proprietary/vendor/lib/liblowi_client.so:system/vendor/lib/liblowi_client.so \
+    vendor/google/seed/proprietary/vendor/lib/libquipc_os_api.so:system/vendor/lib/libquipc_os_api.so \
+    vendor/google/seed/proprietary/vendor/lib/libulp2.so:system/vendor/lib/libulp2.so \
+    vendor/google/seed/proprietary/vendor/lib/libxtadapter.so:system/vendor/lib/libxtadapter.so \
+    vendor/google/seed/proprietary/vendor/lib/libxtwifi_ulp_adaptor.so:system/vendor/lib/libxtwifi_ulp_adaptor.so \
     vendor/google/seed/proprietary/vendor/lib/libscale.so:system/vendor/lib/libscale.so \
     vendor/google/seed/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
     vendor/google/seed/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:system/vendor/lib/egl/eglSubDriverAndroid.so \
